74974fee0ad926f4022641080d5367ae250b7efe
Mithilesh <154891854+MithileshwaranTP@users.noreply.github.com>
2026-02-04 13:39:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const navLinks = document.getElementById('navLinks');
 menuBtn.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+// Rotating/Typewriter text under hero photo
+const texts = [
+    "Automation Engineer",
+    "Instrumentation Engineer",
+    "Industry 4.0 Enthusiast",
+    "IIoT Specialist"
+];
+
+let index = 0;
+const dynamicText = document.getElementById("dynamic-text");
+
+function rotateText() {
+    dynamicText.textContent = texts[index];
+    index = (index + 1) % texts.length;
+}
+
+// Change text every 2 seconds
+setInterval(rotateText, 2000);
